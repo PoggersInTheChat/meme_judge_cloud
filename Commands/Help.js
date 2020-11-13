96d694e1248f99918bcd228606cf2d1e4ff15ab5
PoggersInTheChat <74301970+PoggersInTheChat@users.noreply.github.com>
2020-11-13 12:50:40 +0100

    Delete Help.js

--- Commands/Help.js
+++
@@ -1,7 +0,0 @@
-module.exports= {
-    name: "help",
-    description: "help command",
-    execute(message, args){
-        message.channel.send("scrivi '%judge' per giudicare la tua meme");
-    }
-}
