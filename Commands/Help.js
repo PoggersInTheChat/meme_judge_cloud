2779e0ebeeccf0b23b316bbf64ba487d8c34877b
PoggersInTheChat <74301970+PoggersInTheChat@users.noreply.github.com>
2020-11-13 12:51:53 +0100

    Add files via upload

---
+++ Commands/Help.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+module.exports= {
+    name: "help",
+    description: "help command",
+    execute(message, args){
+        message.channel.send("scrivi '%judge' per giudicare la tua meme");
+    }
+=======
+module.exports= {
+    name: "help",
+    description: "help command",
+    execute(message, args){
+        message.channel.send("scrivi '%judge' per giudicare la tua meme");
+    }
+>>>>>>> 9b1192156637b394997750a196818647976d5d97
+}
